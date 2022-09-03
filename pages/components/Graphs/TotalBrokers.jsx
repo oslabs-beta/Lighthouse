6d5dc5c62604b24978b01d04f55e93a31927f80f
Brandon <chitangb.chin@gmail.com>
2022-09-03 13:26:59 -0400

    graphs

---
+++ pages/components/Graphs/TotalBrokers.jsx
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from "react";
+
+import {
+    Chart as ChartJS,
+    ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+
+  } from 'chart.js';
+
+  import { Line } from "react-chartjs-2"
+  
+  ChartJS.register(
+    ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+
+  );
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const dummyData = []
+
+let x = (randomIntFromInterval(35, 57))
+let y = (randomIntFromInterval(35, 57))
+let z = (randomIntFromInterval(35, 57))
+let a = (randomIntFromInterval(35, 57))
+
+
+function TotalBrokers() {
+  
+  //Chart Data state
+  const [ chartData, setChartData ] = useState({
+    datasets: []
+  })
+
+
+  //You can configure the legend title and tooltip in the chartOptions
+  const [ chartOptions, setChartOptions ] = useState({
+
+  })
+
+  useEffect(() => {
+    setChartData({
+      // Below is the x axis data
+      labels: ["1", "2", "3", "4"],
+      datasets: [
+        {
+          label: "Total Brokers",
+          // Below is the y axis data
+          data: [x, y, z, a],
+          borderColor: "rgba(53,162,235,0.4)",
+          backgroundColor: "rgba(53,162,235,0.4)"
+        }
+      ]
+    }) 
+    setChartOptions({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top"
+        },
+        title: {
+          display: true,
+          text: "Total Brokers",
+        }
+      },
+    })
+  }, [])
+  
+  
+  return(
+    <div>
+    <Line options={ chartOptions } data={chartData}/>
+    </div>
+  )
+}
+
+
+
+  module.exports = { TotalBrokers }
