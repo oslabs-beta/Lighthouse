ec3143169d81b68a8aacf19c2c795981555152a8
Christian <christian@christianspringer.com>
2022-10-05 21:57:41 -0400

    Forreal updated

--- pages/components/Navbar/navbar.jsx
+++ pages/components/Navbar/navbar.jsx
@@ -3,16 +3,11 @@ import Image from 'next/image'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-// import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-// import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-// import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-// import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-// import AdbIcon from '@mui/icons-material/Adb';
 import LighthouseLogo from '../../../public/LighthouseLogo.png'
 
 const pages = ['Github'];
@@ -29,14 +24,11 @@ export default function ResponsiveAppBar() {
   };
 
 
-  
-
   return (
     <AppBar position="static" style={{backgroundColor: '#aceed9'}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Image  src={LighthouseLogo} alt='logo display' sx={{ display: { width: 20, xs: 'none', md: 'container' }, mr: 1 }} />
-          {/* <LighthouseLogo sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
           <Typography
             variant="h4"
             noWrap
@@ -81,24 +73,6 @@ export default function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          {/* <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Lighthouse
-          </Typography> */}
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
